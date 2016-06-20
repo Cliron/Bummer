943a8e683b9d0200066405ff0bc3a69ae31449a5
Liron <liron.inv@gmail.com>
2016-06-20 08:47:31 +0000

    Created the Pages controller, added Explore, Home, Index and Profile.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+# the Pages controller containes all the code for every page inside of /pages
+class PagesController < ApplicationController
+  
+  #backend code for pages/index
+  def index
+  end
+  
+  #backend code for pages/home
+  def home
+  end
+  
+  #backend code for pages/profile
+  def profile
+  end
+  
+  #backend code for pages/explore
+  def explore
+  end
+end
